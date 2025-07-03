c980ed865b13d39e434fb20647fcc1b40002b5de
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-03 04:27:43 +0300

    new main.c to test .tmpl parser

--- src/main/main.c
+++ src/main/main.c
@@ -1,108 +1,63 @@
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <main.h>
+#include <tmplparser.h>
 #include <unistd.h>
-#include <sys/stat.h>
+#include <linux/limits.h>
 
-FILE* mainfile;
-FILE* headerfile;
-FILE* makefile;
+static char* getExecutablePath(size_t* len) {
+	char* buffer = malloc(PATH_MAX);
 
-int checkC(char* string) {
-	return (strcmp(string, "c") == 0 || strcmp(string, "C") == 0);
+	*len = readlink("/proc/self/exe", buffer, PATH_MAX - 1);
+
+        if (*len == -1ul) {
+          fprintf(stderr, "ERROR: couldn't get executable path\n");
+
+          free(buffer);
+
+          return NULL;
+        }
+
+        buffer[*len - 1] = '\0';
+
+	return buffer;
 }
 
-int checkCpp(char* string) {
-	return (strcmp(string, "cpp") == 0 || strcmp(string, "CPP") == 0 || strcmp(string, "c++") == 0);
-}
+static void changeWD(void) {
+	size_t len;
+	char* path = getExecutablePath(&len);
 
-void createCFile(void) {
-	mainfile = fopen("main.c", "w");
-	fputs("#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char **argv) {\n	printf(\"Hello, world!\");\n}", mainfile);
-	headerfile = fopen("header.h", "w");
-	fputs("#ifndef HEADER\n#define HEADER\n\n\n#endif", headerfile);
-}
-
-void createCppFile(void) {
-	mainfile = fopen("main.cpp", "w");
-	fputs("#include <iostream>\n\nusing namespace std;\n\nint main(int argc, char** argv) {\n	cout << \"Hello, world!\";\n}", mainfile);
-	headerfile = fopen("header.hpp", "w");
-	fputs("#ifndef HEADER\n#define HEADER\n\n\n#endif", headerfile);
-}
-
-void createMakeFile(void) {
-	makefile = fopen("Makefile", "w");
-	fputs("main:\n	gcc -o main main.c\nclean:\n	rm main", makefile);
-}
-
-void makeDirs(char* path, char** argv) {
-	chdir(path);
-
-	if (mkdir(argv[1], 0777) != 0) {
-		fprintf(stderr, "cpm: could not create project: could not create directory at \'%s\': %s", path, strerror(errno));
+	if (path == NULL) {
+		fprintf(stderr, "ERROR: failed to get executable path\n");
 		exit(1);
 	}
 
-	strcat(path, "/");
-	strcat(path, argv[1]);
+	int lastSlash = 0;
+	for (size_t i = 0; i < len; i++) {
+		if (path[i] == '/') {
+			lastSlash = i;
+		}
+	}
+
+	path[lastSlash + 1] = '\0';
 
 	chdir(path);
 }
 
-void createCFiles(char* path, char** argv) {
-	makeDirs(path, argv);
+int main(void) {
+	changeWD();
 
-	createCFile();
-	createMakeFile();
-}
+	TMPLFile* tmplFile = tmpl_loadFile("resources/templates-no-folders.tmpl");
 
-void createCppFiles(char* path, char** argv) {
-	makeDirs(path, argv);
-
-	createCppFile();
-	createMakeFile();
-}
-
-void printUsage(char* programName) {
-	printf("usage: %s <project-name> [options]", programName);
-	printf("\noptions:");
-	printf("\n	c: creates a new C project (default behavior)");
-	printf("\n	cpp/CPP/c++: creates a new C++ project\n");
-}
-
-int main(int argc, char** argv) {
-	char* path = (char*) malloc(512);
-
-	getcwd(path, 256);
-
-	if (argc < 2) {
-		printUsage(argv[0]);
-
-		return 0;
+	if (tmplFile == NULL) {
+		printf("tmpl_loadFile error\n");
+		return 1;
 	}
 
-	if (strcmp(argv[1], "help") == 0) {
-		printUsage(argv[0]);
+	char* contents = tmpl_getContentsOfSection(tmplFile, "makefile", NULL);
 
-		return 0;
+	if (contents == NULL) {
+		printf("tmpl_getContentsOfSection error\n");
+		return 1;
 	}
 
-	if (argc < 3) {
-		createCFiles(path, argv);
-
-		return 0;
-	}
-
-	if (checkC(argv[2])) {
-		createCFiles(path, argv);
-
-		return 0;
-	} else if (checkCpp(argv[2])) {
-		createCppFiles(path, argv);
-
-		return 0;
-	}
-
-	printUsage(argv[0]);
+	printf("contents: \n%s\n", contents);
 }
