18660eabdc9679a289d59a21c69220e05d21ec64
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-04 05:06:49 +0300

    added proper documentation to all functions

--- src/headers/cliswitch.h
+++ src/headers/cliswitch.h
@@ -3,7 +3,21 @@
 
 typedef void (*Func)(void);
 
+/**
+ * Adds a switch and its corresponding callback function to a list of switches
+ * and callbacks.
+ *
+ * @param switchName The name of the switch. Max length of 32 characters.
+ * @param callback The function that's called if the switch is found in argv.
+ *
+ * @return `STATUS_OK` if successful, `STATUS_FAIL` otherwise
+ */
 int addSwitch(const char* switchName, Func callback);
+
+/**
+ * Parses argv and looks for any string that matches a string in the list of
+ * switches added and calls that switch's callback function.
+ */
 void parseArgv(int argc, char** argv);
 
 #endif
--- src/headers/tmplparser.h
+++ src/headers/tmplparser.h
@@ -13,7 +13,24 @@ typedef struct {
 	size_t startIndex;
 } Line;
 
+/**
+ * Loads a .tmpl file from `path`.
+ *
+ * @param path The path of the file to load.
+ *
+ * @return `STATUS_OK` if successful, `STATUS_FAIL` otherwise.
+ */
 TMPLFile* tmpl_loadFile(const char* path);
+
+/**
+ * Searches inside the contents of the provided .tmpl file for a section with
+ * the name `sectionName` and grabs the contents of that section.
+ *
+ * @param tmplFile The .tmpl file.
+ * @param sectionName The name of the section.
+ *
+ * @return The contents of the section requested, `NULL` if not found.
+ */
 char* tmpl_getContentsOfSection(const TMPLFile* tmplFile, const char* sectionName, size_t* length);
 
 #endif
