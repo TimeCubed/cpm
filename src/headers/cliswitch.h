a0338a799da6e6bd051ce1d3ace8c9363865e617
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-05 23:57:59 +0300

    updated documentation to match implementation

--- src/headers/cliswitch.h
+++ src/headers/cliswitch.h
@@ -17,7 +17,13 @@ int addSwitch(const char* switchName, Func callback);
 /**
  * Parses argv and looks for any string that matches a string in the list of
  * switches added and calls that switch's callback function.
+ *
+ * @param argc Argument count.
+ * @param argv Argument vector.
+ *
+ * @return The index in `argv` that points to the first non-switch argument, -1
+ *         if none were found.
  */
-void parseArgv(int argc, char** argv);
+int parseArgv(int argc, char** argv);
 
 #endif
