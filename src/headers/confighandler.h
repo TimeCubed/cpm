472f22116ed11ab7cdb8f96626247c2f3cea7e06
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-13 00:05:58 +0300

    updated and improved confighandler.h documentation

--- src/headers/confighandler.h
+++ src/headers/confighandler.h
@@ -42,31 +42,110 @@ typedef struct {
 } ProjectConfig;
 
 /**
- * Initializes a `ProjectConfig` object on the stack with the given parameters.
+ * Returns a stack-allocated `ProjectConfig` object with default values.
+ * Does not set the returned config as the current config.
  *
- * @param name The project name.
- * @param language The project's language.
- * @param projectStructure The project's structure.
- * @param defaultTemplates Whether to force use default templates or use user-created templates if they exist.
+ * m_error is not set when using this function.
  *
  * @return A `ProjectConfig` object allocated on the stack.
  */
-ProjectConfig config_init(String name, Language language, Structure projectStructure, bool defaultTemplates);
+ProjectConfig config_init(void);
 
 /**
- * Loads all files necessary for the current configuration.
+ * Loads all files necessary for the current configuration. Uses the current
+ * config, set by `config_init` or `config_makeCurrent`.
+ *
+ * m_error is set to `STATUS_FAIL` if there was a problem loading the correct
+ * files, reading from said files, allocating memory, or if there is no current
+ * config. Otherwise, it is set to `STATUS_OK`.
  *
  * @param config A pointer to the project's configuration.
- *
- * @return `STATUS_OK` if successful, `STATUS_FAIL` otherwise.
  */
-int config_loadFiles(ProjectConfig* config);
+void config_loadFiles(void);
 
 /**
- * Frees all strings stored inside `config`.
+ * Frees the old name (if not `NULL`) and sets the name of the current config to
+ * `name`.
+ *
+ * If there is no current config, m_error is set to `STATUS_FAIL` and this
+ * function won't do anything. Otherwise, m_error is set to `STATUS_OK`.
+ *
+ * @param name The new name for the config.
+ */
+void config_setName(String name);
+
+/**
+ * Sets the language of the current config to `language`.
+ *
+ * If there is no current config, m_error is set to `STATUS_FAIL` and this
+ * function won't do anything. Otherwise, m_error is set to `STATUS_OK`.
+ *
+ * @param language The new language for the config.
+ */
+void config_setLanguage(Language language);
+
+/**
+ * Sets the structure of the current config to `projectStructure`.
+ *
+ * If there is no current config, m_error is set to `STATUS_FAIL` and this
+ * function won't do anything. Otherwise, m_error is set to `STATUS_OK`.
+ *
+ * @param projectStructure The new structure for the config.
+ */
+void config_setStructure(Structure projectStructure);
+
+/**
+ * Sets whether to force use the default templates.
+ *
+ * If there is no current config, m_error is set to `STATUS_FAIL` and this
+ * function won't do anything. Otherwise, m_error is set to `STATUS_OK`.
+ *
+ * @param defaultTemplates `true` to force using default templates,
+ *                         `false` otherwise.
+ */
+void config_setDefaultTemplates(bool defaultTemplates);
+
+/**
+ * Frees all strings stored inside the current config.
+ *
+ * If there is no current config, m_error is set to `STATUS_FAIL` and this
+ * function won't do anything. Otherwise, m_error is set to `STATUS_OK`.
  *
  * @param config The configuration to free.
  */
-void config_free(ProjectConfig config);
+void config_freeCurrent(void);
+
+/**
+ * Makes the given project configuration the current configuration to be used in
+ * other functions.
+ *
+ * m_error is set to `STATUS_FAIL` if `config` is `NULL`.
+ *
+ * @param config The pointer to a config to set as the current config.
+ */
+void config_makeCurrent(ProjectConfig* config);
+
+/**
+ * Returns the current config (read-only).
+ *
+ * If there is no current config, m_error is set to `STATUS_FAIL`, and returns a
+ * default config from `config_init`. Otherwise, m_error is set to `STATUS_OK`.
+ */
+ProjectConfig config_getCurrent(void);
+
+/**
+ * Checks if there is a current config bound.
+ *
+ * m_error is not set when using this function.
+ *
+ * @return `true` if there is a current config, `false` otherwise.
+ */
+bool config_isCurrent(void);
+
+/**
+ * Returns the status of the last run config handler function.
+ * `STATUS_OK` if successful, `STATUS_FAIL` otherwise.
+ */
+int config_checkError(void);
 
 #endif
