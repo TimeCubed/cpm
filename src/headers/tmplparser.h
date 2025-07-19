f6e0eab16f07f0b6f6bb3cabcc6145f6268956e6
TimeSquared <48620951+TimeCubed@users.noreply.github.com>
2025-07-19 22:25:37 +0300

    updated documentation

--- src/headers/tmplparser.h
+++ src/headers/tmplparser.h
@@ -2,6 +2,7 @@
 #define TMPL_PARSER
 
 #include <main.h>
+#include <stdbool.h>
 
 typedef struct {
 	const char* contents;
@@ -13,6 +14,26 @@ typedef struct {
 	size_t startIndex;
 } Line;
 
+// error handling struct that is a shameless copy of C++23's std::expected
+typedef struct {
+	union {
+		TMPLFile* tmplFile;
+		const char* errorMessage;
+	};
+
+	bool hasValue;
+} LoaderStatus;
+
+// this is why c++ has templates
+typedef struct {
+	union {
+		char* sectionContents;
+		const char* errorMessage;
+	};
+
+	bool hasValue;
+} ParserStatus;
+
 /**
  * Loads a .tmpl file from `path`.
  * 
@@ -22,10 +43,19 @@ typedef struct {
  *
  * @param path The path of the file to load.
  *
- * @return A pointer to the TMPLFile object with the contents and the file
- *         length. `NULL` if an error occured while loading file contents.
+ * @return A `LoaderStatus` object, indicating the status of the operation.
+ *         If the object's `hasValue` field is set to `true`, that indicates
+ *         that the operation was successful, and you may access the object's
+ *         `tmplFile` field to obtain a pointer to a heap-allocated `TMPLFile`
+ *         object. If `hasValue` is `false`, that indicates that an error
+ *         occured, and you may access `errorMessage` to get more details on the
+ *         error.
+ *
+ *         Note: The `tmplFile` and `errorMessage` fields are wrapped in a
+ *         union, meaning that if one is set, the other is pointing to invalid
+ *         data.
  */
-TMPLFile* tmpl_loadFile(const char* path);
+LoaderStatus tmpl_loadFile(const char* path);
 
 /**
  * Searches inside the contents of the provided .tmpl file for a section with
@@ -36,8 +66,18 @@ TMPLFile* tmpl_loadFile(const char* path);
  * @param length The int pointed to by this parameter is set to the length of
  *               the section's contents.
  *
- * @return The contents of the section requested, `NULL` if not found.
+ * @return A `ParserStatus` object, indicating the status of the operation.
+ *         If the object's `hasValue` field is set to `true`, that indicates
+ *         that the operation was successful, and you may acces the object's
+ *         `sectionContents` field to obtain a pointer to the contents of the
+ *         requested section. If `hasValue` is false, that indicates that an
+ *         error occurred, and you may access `errorMessage` to get more details
+ *         on the error.
+ *
+ *         Note : The `sectionContents` and `errorMessage` fields are wrapped in
+ *         a union, meainig that if one is set, the other is likely pointing to
+ *         invalid data.
  */
-char* tmpl_getContentsOfSection(const TMPLFile* tmplFile, const char* sectionName, size_t* length);
+ParserStatus tmpl_getContentsOfSection(const TMPLFile* tmplFile, const char* sectionName, size_t* length);
 
 #endif
